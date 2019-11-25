7b491b410246e79afdf36370bfc1da397b888923
程石亮 <shiliangcheng@hk01.com>
2019-11-25 14:23:15 +0800

    feat: new version 0.3.0

---
+++ CSLCommonLibrary/NSObject+PresentAnimation.h
@@ -0,0 +1,19 @@
+//
+//  NSObject+PresentAnimation.h
+//  CSLCommonLibrary
+//
+//  Created by SZDT00135 on 2019/11/21.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (PresentAnimation)
+
+@end
+
+NS_ASSUME_NONNULL_END
